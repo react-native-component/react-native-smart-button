2490ab2ef0d43ee18c6baf29fa5ffb6b56204495
cyqresig <moonsunfall@aliyun.com>
2016-09-13 16:00:06 +0800

    added constants

---
+++ constants.js
@@ -0,0 +1,20 @@
+
+export const touchableTypes = {
+    fade: 0,
+    fadeContent: 1,
+    highlight: 2,
+    blur: 3,
+}
+
+export const SYSTEM_OPACITY = 0.2
+export const NOOP = () => {}
+export const EMPTY_OBJECT = {}
+
+export const DEFAULT_SHADOW_COLOR = '#fff'
+export const DEFAULT_SHADOW_OPACITY = 1
+export const DEFAULT_SHADOW_ANIMATED = true
+export const DEFAULT_SHADOW_DURATION = 150
+
+export default {
+    touchableTypes,
+}
